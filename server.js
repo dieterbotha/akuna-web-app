0ac26cb711020bb9886e838269ce3a11f58cd13c
dieterbotha <dieter@bothamail.com>
2017-06-11 10:25:18 +1000

    add a few routes and handlebars

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const hbs = require('hbs');
 
-app.get('/', function (req, res){
-  res.send('Hello Boet!')
-})
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// Some middlewhere
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  console.log(`${now}: ${req.method} ${req.url}`)
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello Boet!',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// /bad - send back JSON with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(3000, function(){
   console.log('App listening on port 3000')
