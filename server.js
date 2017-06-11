9f28306d249d7233541bd916a0abd640f962e31d
dieterbotha <dieter@bothamail.com>
2017-06-11 11:05:36 +1000

    added process.env.PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+var port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -43,6 +44,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log('App listening on port 3000')
 })
